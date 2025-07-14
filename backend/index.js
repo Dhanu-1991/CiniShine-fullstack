a1979841956d355b6d9a12488c7918f92c30d733
Dhanush Kumar <dhanushkumarvr019@gmail.com>
2025-07-14 20:35:14 +0530

    Update: latest changes to frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,30 @@
 import express from "express";
-import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRouter from "./routes/authRoutes/authRouter.js";
 import errorHandlingMiddleware from "./middlewares/errorHandlingMiddleware.js";
 import router from "./routes/paymentRoutes/cashfree.js";
-
+import cors from "cors";
 dotenv.config();
 let app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://cini-shine-fullstack-hru4-git-main-dhanu-1991s-projects.vercel.app"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173", // frontend origin
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS: " + origin));
+    }
+  },
+  credentials: true,
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
